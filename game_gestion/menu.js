aac1fe6ee1bd66e763e6d1ec1a80a582408b12ef
Hugo-Lourenco <hugo.lourenco@ynov.com>
2026-03-07 17:51:26 +0100

    add the main menu

--- game_gestion/menu.js
+++ game_gestion/menu.js
@@ -1,15 +1,33 @@
-// Fonction appelée quand le joueur clique sur "JOUER"
+// Function call when the player click on play
 function lancerMenu() {
-    // 1. On cache le menu
     document.getElementById('main-menu').style.display = 'none';
-    
-    // 2. On affiche l'interface du jeu
     document.getElementById('game-ui').style.display = 'block';
     
-    // 3. On déclenche le moteur de jeu de tes potes
     if (typeof window.demarrerLeJeu === "function") {
         window.demarrerLeJeu(); 
     } else {
         console.error("Erreur : La connexion avec map.js n'est pas faite !");
     }
 }
+
+// skip menu
+window.addEventListener('DOMContentLoaded', () => {
+    // we look the url
+    const urlParams = new URLSearchParams(window.location.search);
+    
+    // if it contains "?skipMenu=true"
+    if (urlParams.get('skipMenu') === 'true') {
+        
+        // we hide the black screen instantly
+        document.getElementById('main-menu').style.display = 'none';
+        document.getElementById('game-ui').style.display = 'block';
+        
+        // we wait the loading of map.js and we start
+        let attenteJeu = setInterval(() => {
+            if (typeof window.demarrerLeJeu === "function") {
+                window.demarrerLeJeu(); // auto start
+                clearInterval(attenteJeu); // stop searching 
+            }
+        }, 100); // verification 
+    }
+});
